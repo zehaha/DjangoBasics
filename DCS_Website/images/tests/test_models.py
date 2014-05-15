664a13ef5fba2320e8f35720cb0799dbe874f7d9
XietrZ <sxietrz@yahoo.com>
2014-05-15 14:38:39 -0700

    tests for news,people,images

---
+++ DCS_Website/images/tests/test_models.py
@@ -0,0 +1,13 @@
+from django.template.loader import render_to_string
+from django.test import TestCase, Client
+from images.models import Image
+from django.conf.urls import patterns, url
+
+class ImageModelTest(TestCase):
+
+	def create_image(self,caption='asdasd',shown_in='asdasd'):
+		return Image.objects.create(caption=caption,shown_in=shown_in)
+	def test_image_creation(self):
+		image = self.create_image()
+		self.assertTrue(isinstance(image,Image))
+		self.assertEqual(image.__str__(),(image.caption))
---
+++ DCS_Website/news/tests/test_models.py
@@ -0,0 +1,13 @@
+from django.template.loader import render_to_string
+from django.test import TestCase, Client
+from news.models import NewsArticle
+from django.conf.urls import patterns, url
+
+class NewsModelTest(TestCase):
+
+	def create_news(self,pub_date='2014-04-30',title='asdasd',body='asdasd'):
+		return NewsArticle.objects.create(pub_date=pub_date,title=title,body=body)
+	def test_news_creation(self):
+		news = self.create_news()
+		self.assertTrue(isinstance(news,NewsArticle))
+		self.assertEqual(news.__str__(),(news.title))
