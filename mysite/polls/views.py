264f6f84646b54e7e808b8bc7fd7bc98ff0cd64a
zehaha <linux@philip.(none)>
2014-07-03 12:41:55 +0800

    Fifth Commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.views import generic
 from polls.models import Choice, Poll
+from django.utils import timezone	
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -10,12 +11,19 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
-
+        return Poll.objects.filter(
+       		pub_date__lte=timezone.now()
+    	).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
+	
+def get_queryset(self):
+    """
+    Excludes any polls that aren't published yet.
+    """
+    return Poll.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
